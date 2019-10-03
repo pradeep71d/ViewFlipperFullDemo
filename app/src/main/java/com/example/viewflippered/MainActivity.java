8268a8a4244c37c64da5c90b214ecba7835dbab9
sandeep <sandeep@ashmar.in>
2019-10-03 10:51:33 +0530

    add

---
+++ app/src/main/java/com/example/viewflippered/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.viewflippered;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+    ViewFlipper viewFlipper;
+    Button button1, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewFlipper = findViewById(R.id.viewflipper);
+        button1 = findViewById(R.id.b1);
+        button2 = findViewById(R.id.b2);
+        TextView textView = new TextView(this);
+        textView.setText("this is thirsd ");//this is dynamically generated textview
+        viewFlipper.addView(textView); // we put all views inside viewflipper which views we want to show,except both buttons
+        button1.setOnClickListener(new View.OnClickListener() { //click event on previous button
+            @Override
+            public void onClick(View v) {
+                viewFlipper.showPrevious();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() { // click event on next button
+            @Override
+            public void onClick(View v) {
+                viewFlipper.showNext();
+            }
+        });
+    }
+}
+
+
